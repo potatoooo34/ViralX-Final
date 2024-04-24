8939b5a8d31391587b66a76836f612b8571b1725
potatoooo34 <108672710+potatoooo34@users.noreply.github.com>
2024-04-24 11:21:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -87,62 +87,6 @@ public class Main {
 
         }
         sc.close();
-        all_data data = new all_data();
-        Country country = data.getCountry();
-        data.readInputFromFile("inputFiles/input.txt");
-        data.readInputFromFile("inputFiles/hospital.txt", country);
-        Patient[] patients = data.getPatients();
-        Hospital[] hospitals = data.getHospitals();
-        State[] states = data.getStates();
 
-        System.out.println("Names of Patients:");
-        for (Patient patient : patients) {
-            if (patient != null) {
-                System.out.println(patient.getName());
-            }
-        }
-
-        System.out.println("\n\nNames of Hospitals:");
-        for (Hospital hospital : hospitals) {
-            if (hospital != null) {
-                System.out.println(hospital.getName());
-            }
-        }
-
-        System.out.println("\n\nNames of States:");
-        for (State state : states) {
-            if (state != null) {
-                System.out.println(state.getName());
-            }
-        }
-        int i = 0;
-        int j = 0;
-        // main logic to put patient in hospital
-        while(i < hospitals.length)
-        {
-            //System.out.println("LAST KAAM BAAKI");
-            while(j<patients.length)
-            {
-            if(patients[j] == null || hospitals[i] == null)
-            {
-                break;
-            }
-            patients[j].isConfirmedCase();
-            if(patients[j].getStatus().equals("Infected") && !patients[j].getAdmitStatus())
-            {
-                if(hospitals[i].addSubObj(patients[j])){
-                patients[j].setAdmitStatus(true);
-                j++;
-                }
-                else{
-                break;
-                }
-            }
-            }
-            i++;
-        }
-        country.displayData();
-        // Method to read input from file and create patients and contacts
-    }
-    
-}
+        //getting data from all the 
+        
